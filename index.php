f34a9c553d9da5d05ededb577238c1f16a6d6421
Attila Szeremi <attila@szeremi.org>
2019-08-09 14:20:46 +0200

    Move MobillsApp logic to its class

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 declare(strict_types=1);
 
-use amcsi\BankStatementMerger\Transaction\Transaction;
-use amcsi\BankStatementMerger\Transaction\TransactionHistory;
-use League\Csv\CharsetConverter;
-use League\Csv\Reader;
+use amcsi\BankStatementMerger\Readers\MobillsAppReader;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -25,22 +22,6 @@ if (!$csvFile) {
     exit(1);
 }
 
-$reader = Reader::createFromPath("$dir/$csvFile");
-$reader->setHeaderOffset(0);
-$reader->setDelimiter(';');
-CharsetConverter::addTo($reader, 'utf-16le', 'utf-8');
-$amount = 0;
-$index = -1;
-$transactions = [];
-$transactions[] = new Transaction((double) $_ENV['MOBILLSAPP_STARTING_AMOUNT'], new DateTimeImmutable('2016-06-01 00:00:00'));
-foreach ($reader->getRecords() as $row) {
-    $rowAmount = (float) str_replace(',', '.', $row['Valor']);
-    if (!$rowAmount) {
-        throw new RuntimeException('Row amount is 0');
-    }
-    $transactions[] = new Transaction($rowAmount, DateTimeImmutable::createFromFormat('d/m/Y', $row['Fecha']));
-}
-
-$transactionHistory = new TransactionHistory($transactions);
+$transactionHistory = (new MobillsAppReader())->buildTransactionHistory("$dir/$csvFile");
 
 echo number_format($transactionHistory->calculateTotalAmount(), 2) . "\n";
--- src/Readers/MobillsAppReader.php
+++ src/Readers/MobillsAppReader.php
@@ -3,10 +3,32 @@ declare(strict_types=1);
 
 namespace amcsi\BankStatementMerger\Readers;
 
+use amcsi\BankStatementMerger\Transaction\Transaction;
+use amcsi\BankStatementMerger\Transaction\TransactionHistory;
+use DateTimeImmutable;
+use League\Csv\CharsetConverter;
+use League\Csv\Reader;
+use RuntimeException;
+
 class MobillsAppReader
 {
-    public function read(string $contents)
+    public function buildTransactionHistory(string $filename)
     {
+        $reader = Reader::createFromPath($filename);
+        $reader->setHeaderOffset(0);
+        $reader->setDelimiter(';');
+        CharsetConverter::addTo($reader, 'utf-16le', 'utf-8');
+        $transactions = [];
+        $transactions[] = new Transaction((double) $_ENV['MOBILLSAPP_STARTING_AMOUNT'],
+            new DateTimeImmutable('2016-06-01 00:00:00'));
+        foreach ($reader->getRecords() as $row) {
+            $rowAmount = (float) str_replace(',', '.', $row['Valor']);
+            if (!$rowAmount) {
+                throw new RuntimeException('Row amount is 0');
+            }
+            $transactions[] = new Transaction($rowAmount, DateTimeImmutable::createFromFormat('d/m/Y', $row['Fecha']));
+        }
 
+        return new TransactionHistory($transactions);
     }
 }
