c49986b6f5eb1fd8342d59f55423553e7c1e0843
Attila Szeremi <attila@szeremi.org>
2019-08-09 14:03:42 +0200

    Use League\Csv package

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 
+use League\Csv\CharsetConverter;
+use League\Csv\Reader;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::create(__DIR__);
@@ -20,19 +23,15 @@ if (!$csvFile) {
     exit(1);
 }
 
-$f = fopen("$dir/$csvFile", 'rb');
+$reader = Reader::createFromPath("$dir/$csvFile");
+$reader->setHeaderOffset(0);
+$reader->setDelimiter(';');
+CharsetConverter::addTo($reader, 'utf-16le', 'utf-8');
 $amount = 0;
 $index = -1;
 $amount = (double) $_ENV['MOBILLSAPP_STARTING_AMOUNT']; // Starting balance.
-while ($line = fgets($f)) {
-    ++$index;
-    if (!$index) {
-        // Skip first row.
-        continue;
-    }
-    $row = str_getcsv(mb_convert_encoding($line, 'utf-8', 'utf-16'), ';', '"');
-
-    $rowAmount = (float) str_replace(',', '.', $row[2]);
+foreach ($reader->getRecords() as $row) {
+    $rowAmount = (float) str_replace(',', '.', $row['Valor']);
     if (!$rowAmount) {
         throw new RuntimeException('Row amount is 0');
     }
